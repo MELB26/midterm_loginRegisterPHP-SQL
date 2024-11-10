baa44fcd388351b43daf4916e146b8fbedd58183
MELB26 <90427804+MELB26@users.noreply.github.com>
2024-11-10 23:26:30 +0800

    Update data.sql

--- RINON/data.sql
+++ RINON/data.sql
@@ -6,7 +6,15 @@ CREATE TABLE casino (
 CREATE TABLE casino_details (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(50),
-    date_added date,
+    date_added DATETIME DEFAULT CURRENT_TIMESTAMP,
     Branch VARCHAR(50),
+    casino_cat_id INT,
+);
+
+CREATE TABLE user_passwords (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(50),
+    date_added DATETIME DEFAULT CURRENT_TIMESTAMP,
     casino_cat_id INT,
 );
